0a76f7122a945f3de540f01ca4d6d1c0a2e6dafe
admin <admin@admin-PC>
2020-10-11 17:52:35 +0530

    First Commit..Added Sample1.java

---
+++ JavaGit1/src/sample/Sample1.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello..Welcome to Java.");
+	}
+
+}
