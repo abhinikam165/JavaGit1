3c74620b954c108e2f8c5fe769e7e4b9258067f2
admin <admin@admin-PC>
2020-10-11 17:58:16 +0530

    Addedd Addition.java

---
+++ JavaGit1/src/sourcecode/Addition.java
@@ -0,0 +1,14 @@
+package sourcecode;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 10;
+		int b = 11;
+		
+		System.out.println("Addition is: "+(a+b));
+	}
+
+}
